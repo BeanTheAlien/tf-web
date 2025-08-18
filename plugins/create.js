287ac3aac4c69933667f2d1b8b66bed955dfa95c
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-08-18 08:29:31 -0500

    Create updates

---
+++ plugins/create.js
@@ -0,0 +1,30 @@
+const startup = document.createElement("div");
+startup.innerHTML = `<button id="is-json">Create from JSON upload</button> or <button id="is-fields">Create from fields</button>`;
+document.body.appendChild(startup);
+const isjson = document.getElementById("is-json");
+const isfields = document.getElementById("is-fields");
+isjson.addEventListener("click", () => {
+    document.body.removeChild(startup);
+    createFromJSON();
+});
+isfields.addEventListener("click", () => {
+    document.body.removeChild(startup);
+    createFromFields();
+});
+
+function createFromJSON() {
+    const upload = document.createElement("input");
+    upload.type = "file";
+    document.body.appendChild(upload);
+    upload.addEventListener("change", (e) => {
+        const file = e.target.files[0];
+        try {
+            const data = JSON.parse(file);
+        } catch(err) {
+            const div = document.createElement("div");
+            div.innerHTML = `JSON file parse failed: ${err}`;
+            document.body.appendChild(div);
+        }
+    });
+}
+function createFromFields() {}
