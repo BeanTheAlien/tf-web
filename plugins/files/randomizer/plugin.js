8299ae83a5ec15292b2444972f5a8f6f55828bb1
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-08-19 09:59:33 -0500

    Updated randomizer plugin file

---
+++ plugins/files/randomizer/plugin.js
@@ -0,0 +1,16 @@
+const plugin = {
+    "name": "Randomizer",
+    "descrip": "A port of the randomizer plugin from TF2.",
+    "author": "The TF-Web Team",
+    "version": "0.0.0",
+    "events": {
+        "game": {
+            "merc": {
+                "spawn": ""
+            }
+        }
+    },
+    "commands": {}
+};
+
+export { plugin };
