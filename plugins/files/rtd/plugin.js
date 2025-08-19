23a29440506d3ba67ea116984d2a57232517dc56
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-08-19 09:28:58 -0500

    Added and updated RTD plugin file

---
+++ plugins/files/rtd/plugin.js
@@ -0,0 +1,12 @@
+const plugin = {
+    "name": "RTD",
+    "descrip": "A port of the RTD plugin from TF2.",
+    "author": "The TF-Web Team",
+    "version": "0.0.0",
+    "events": {},
+    "commands": {
+        "rtd": ""
+    }
+};
+
+export { plugin };
